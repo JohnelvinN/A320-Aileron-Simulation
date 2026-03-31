b4dc7373803ea80875dbe8df76f970b368836f0a
John Elvin <147663807+JohnelvinN@users.noreply.github.com>
2026-03-31 17:55:16 +0200

    Changing variable names to match new schem

--- Displacement_vsDeflection_angle_the_real_one.m
+++ Displacement_vsDeflection_angle_the_real_one.m
@@ -3,15 +3,15 @@ clc;
 close all;
 
 %% The Main Variables Declared
-r=0.15;  % [m] Distance from hinge to actuator connection (DO to EO)
-Ro=020;   % [m] Distance from hinge to the actuator fixed point (DO to FO)
-Bo_deg=30;  % [degrees] Initial Angle between DOFO and DOEO
+r=0.10;  % [m] Distance from hinge to actuator connection 
+D=0.085;   % [m] Distance from hinge to the actuator fixed point
+Bo_deg=60;  % [degrees] Initial Angle between D and r
 
 Bo=Bo_deg*pi/180;
 
 %% Now to Claculate the Initial Length of the actuator (Lo) using the cosine rule
-Lo=sqrt(r^2+Ro^2-2*r*Ro*cos(Bo));
-fprintf('Neutral actuator length Lo =%.1f mm\n',Lo*1000);
+Po=sqrt(r^2+D^2-2*r*D*cos(Bo));
+fprintf('Neutral actuator length Lo =%.1f mm\n',Po*1000);
 
 %% Now just to declare the ranges of the aileron deflection
 theta_min_deg=-25;
@@ -20,15 +20,15 @@ theta_deg=linspace(theta_min_deg,theta_max_deg,100);
 
 theta_rad=theta_deg*pi/180;
 
-L=zeros(size(theta_rad));  % Array that will hold the values of L (new Lengths of the actuator) as they are being calculated in the for loop
+P=zeros(size(theta_rad));  % Array that will hold the values of P (new Lengths of the actuator) as they are being calculated in the for loop
 
 
 for i=1:length(theta_rad)
     B =Bo+theta_rad(i);               % New Angle calculated
-    L(i)=sqrt(r^2+Ro^2-2*r*Ro*cos(B));     %New Actuator length
+    P(i)=sqrt(r^2+D^2-2*r*D*cos(B));     %New Actuator length
 end
 
-x=L-Lo;  % The Extension
+x=P-Po;  % The Extension
 x_mm=x *1000;
 
 figure('Position',[100 100 800 500]);
